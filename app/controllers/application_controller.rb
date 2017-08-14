1093bb08d97f005c3738c8752dd3b87326a25d9b
Charlie Gaines <beaugaines@gmail.com>
2017-08-14 19:11:06 -0400

    profile - wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:role, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def after_sign_up_path_for(resource)
+    new_user_profile_path(resource)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+
+  resources :users, only: [] do
+    resources :profiles, only: [:new, :create]
+    get 'profile', to: 'profiles#show'
+  end
+
+
   root to: 'home#index'
 end
